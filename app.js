362f7eb5174b98bf489be776dadbdc0f6a93c8c9
hmrvc <hmrvc@yahoo.com.tw>
2022-01-09 15:11:32 +0800

    Build init web

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const { engine } = require('express-handlebars') 
+
+
+const app = express()
+const port = 3000
+
+app.engine('handlebars', engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`)
+})
