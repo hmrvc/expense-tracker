e12dc7f2ffe028d80c4eea1d0afe97a69cc142db
hmrvc <hmrvc@yahoo.com.tw>
2022-01-13 17:58:51 +0800

    Add post register router

--- routes/modules/user.js
+++ routes/modules/user.js
@@ -1,12 +1,38 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../../models/user')
 
+//導向登入頁面
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
+//導向註冊頁面
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+//取得註冊頁面資料 做到這裡
+router.post('/register', (req, res) => {
+  const {name, email, password, confirmPassword} = req.body
+  User.findOne({email}).then(user => {
+    if (user) {
+      console.log('User is already exist.')
+      res.render('register', {
+        name,
+        email,
+      })
+    } else {
+      return User.create({
+        name,
+        email,
+        password
+      })
+      .then(() => res.redirect('/'))
+      .catch(error => console.log(error))
+    }
+  })
+
 })
 
 module.exports = router
