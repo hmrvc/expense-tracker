e593e1cfe44fa2fa74f987a7232b9325056a8f04
hmrvc <hmrvc@yahoo.com.tw>
2022-01-13 21:05:50 +0800

    Add login & logout function

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override')
 
 const routes = require('./routes')
 require('./config/mongoose')
+const usePassport = require('./config/passport')
 
 const app = express()
 const port = 3000
@@ -16,12 +17,14 @@ app.set('view engine', 'hbs')
 app.use(session({
   secret: 'TTT',
   resave: false,
-  saveUninitial: true
+  saveUninitialized: true
 }))
 
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+
+usePassport(app)
 app.use(routes)
 
 
--- routes/modules/user.js
+++ routes/modules/user.js
@@ -2,10 +2,22 @@ const express = require('express')
 const router = express.Router()
 const User = require('../../models/user')
 const bcrypt = require('bcryptjs')
+const passport = require('passport')
 
 //導向登入頁面
 router.get('/login', (req, res) => {
   res.render('login')
+})
+//驗證登入資訊
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/user/login'
+}))
+
+//登出功能
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/user/login')
 })
 
 //導向註冊頁面
@@ -13,7 +25,7 @@ router.get('/register', (req, res) => {
   res.render('register')
 })
 
-//取得註冊頁面資料 做到這裡
+//取得註冊頁面資料 
 router.post('/register', (req, res) => {
   const {name, email, password, confirmPassword} = req.body
   User.findOne({email}).then(user => {
