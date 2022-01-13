347e301f7aca36cc67c5777db56e2ab576ccd354
hmrvc <hmrvc@yahoo.com.tw>
2022-01-13 10:13:20 +0800

    Add index edit create page

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
-app.engine('handlebars', engine({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
+app.engine('hbs', engine({defaultLayout: 'main', extname: 'hbs'}))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/edit', (req, res) => {
+  res.render('edit')
+})
+app.get('/create', (req, res) => {
+  res.render('create')
 })
 
 app.listen(port, () => {
