99f2f711a3b31a19c8f9d5301bdc197162732cc8
hmrvc <hmrvc@yahoo.com.tw>
2022-01-16 12:25:17 +0800

    Add search by category

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -21,6 +21,25 @@ router.get('/', (req, res) => {
    .catch(error => console.log(error))
 })
 
+//預覽選擇的類別
+router.get('/search', (req, res) => {
+  const userId = req.user._id
+  let totalAmount = 0
+  const categorySet = req.query.category
+  return Record.find({ category: categorySet })
+    .lean()
+  //取得關聯資料庫項目
+    .populate('categoryId')
+    .lean()
+    .sort({ _id: 'asc'})
+    .then(records => {
+      records.forEach(item => {
+        totalAmount += item.amount
+      })
+      res.render('index', {records, totalAmount}
+    )})
+    .catch(error => console.log(error))
+})
 
 
 module.exports = router
